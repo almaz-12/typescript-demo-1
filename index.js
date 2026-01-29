016f963b02d5961a4b41d2de12dbb185125f1e2b
Almaz Khabibullin <adreams2016@yandex.ru>
2026-01-29 12:43:31 +0300

    Использование типов

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello TypeScript!");
+let rev = 1000;
+let bonus = 500;
+let res = rev + bonus;
+console.log(res);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-console.log("Hello TypeScript!");
+let rev: number = 1000;
+let bonus: number = 500;
+
+let res: number = rev + bonus;
+console.log(res);
