8daf8a1214f058bd2537fc3ea85225e55689b2f3
Almaz Khabibullin <adreams2016@yandex.ru>
2026-01-30 08:10:13 +0300

    Readonly

--- index.ts
+++ index.ts
@@ -1,13 +1,12 @@
-const skills: [number, string] = [1, 'dev'];
+const skills: readonly [number, {name: string}] = [1, {name: 'dev'}];
 
 const [id, skillName] = skills;
 
 console.log(id); // 1
-console.log(skillName); // dev
+console.log(skillName); // {name: 'dev'}
 
-// так делать можно, но прочитать нельзя
+// не получится сделать с readonly
 skills.push('test')
-skills[2] // тип кортежа длиной 2 не имеет элемент с индексом 2
 
-// неограниченное количество
-const skills1: [number, string, ...boolean[]] = [1, 'dev', true, false];
+// !!! не работает для вложенного объекта
+skills[1].name = 'another'
