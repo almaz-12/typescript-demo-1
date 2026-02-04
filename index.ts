acb3d546efcc9528f1e0aeac5ddf04216d9b511a
Almaz Khabibullin <adreams2016@yandex.ru>
2026-02-04 08:39:31 +0300

    Literal Types

--- index.ts
+++ index.ts
@@ -1,29 +1,5 @@
-function logId(id: string | number | boolean) {
-	if(typeof id === 'string') {
-		id.toUpperCase();
-	} else if(typeof id === 'number') {
-		console.log(id);
-	} else {
-		console.log(id);
-	}
+function fetchData(url: string, method: 'post' | 'get'): 1 | 0  {
+	return 1;
 }
 
-logId(3);
-
-function error(id: string | string[]) {
-	if(Array.isArray(id)) {
-		console.log(id);
-	} else {
-		console.log(id);
-	}
-}
-
-error('3');
-
-function logObject(obj: { a: number } | { b : string}) {
-	if('a' in obj) {
-		console.log(obj.a);
-	} else {
-		console.log(obj.b);
-	}
-}
+fetchData('asd', 'post')
