fdcd1ac57de3742ba87ef5d60cc0d4658acda1bb
Almaz Khabibullin <adreams2016@yandex.ru>
2026-02-04 09:04:51 +0300

    Type Aliases

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
-function fetchData(url: string, method: 'post' | 'get'): 1 | 0  {
+// 1. простой тип
+type ID = number;
+
+// 2. литеральный
+type Status = 'pending' | 'cancel';
+type Method = 'post' | 'get';
+
+
+function fetchData(url: string, method: Method, id: ID): 1 | 0  {
 	return 1;
 }
 
-fetchData('asd', 'post')
+// 3.
+type User = {
+	id: number;
+	name: string;
+	role?: 'admin' | 'manager';
+}
+
+const user: User = {
+	id: 1,
+	name: 'asd',
+}
